417ad4b463860971e2ac8d0a532357b6fd96bde2
Ati-bre <ATI-BRE@ATI-BRE-MacBook-Pro.local>
2026-05-01 02:19:53 +0100

    fix: simplify callback route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,15 +1,8 @@
-import { createClient } from '@/lib/supabase/server'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
   const type = searchParams.get('type')
-
-  if (code) {
-    const supabase = createClient()
-    await supabase.auth.exchangeCodeForSession(code)
-  }
 
   if (type === 'recovery') {
     return NextResponse.redirect(`${origin}/auth/reset`)
